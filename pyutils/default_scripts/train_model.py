d2bba72a744546c2c368b1affbe52989b347615a
Raul <rchfe23@gmail.com>
2020-12-02 14:04:03 -0500

    train_model.py: get logger name (not just use __name__), add comments, andd compute score on test

--- pyutils/default_scripts/train_model.py
+++ pyutils/default_scripts/train_model.py
@@ -3,9 +3,11 @@
 import logging.config
 from logging import NullHandler
 
+from sklearn.metrics import accuracy_score
+
 from pyutils import genutils as ge, mlutils as ml
 
-logger = logging.getLogger(__name__)
+logger = logging.getLogger(ge.get_logger_name(__name__, __file__))
 logger.addHandler(NullHandler())
 
 logger_data = logging.getLogger('data')
@@ -13,26 +15,39 @@ logger_data.addHandler(NullHandler())
 
 
 def main():
-    global logger
+    # ---------------------------------
+    # Setup logging and get config dict
+    # ---------------------------------
     bp = ge.ConfigBoilerplate(__file__)
     cfg_dict = bp.get_cfg_dict()
-    # Important: remove next line and global logger
-    logger = bp.get_logger()
 
+    # ---------
     # Load data
-    data = ml.Datasets(**cfg_dict)
+    # ---------
+    data = ml.Dataset(**cfg_dict)
 
+    # ---------
     # Get model
+    # ---------
     clf = ml.get_model(**cfg_dict['model'])
 
-    # Train and get preds
+    # -----------
+    # Train model
+    # -----------
     logger.info("Training model")
     logger_data.debug(f"{clf}")
     clf.fit(data.X_train, data.y_train)
     score = clf.score(data.X_train, data.y_train)
     logger.info(f"Score on train: {score}")
+
+    # -----------------------
+    # Get predictions on test
+    # -----------------------
     logger.info("Getting predictions from test data")
     predictions = clf.predict(data.X_test)
+    if data.y_test is not None:
+        test_score = accuracy_score(data.y_test, predictions)
+        logger.info(f"Score on test: {test_score}")
 
 
 if __name__ == '__main__':
