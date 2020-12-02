2189561aeb581eb0794d3bb64d88082f9d3ae1c2
Raul <rchfe23@gmail.com>
2020-12-02 04:13:53 -0500

    __init__: add global variables like SKLEARN_MODULES

--- pyutils/__init__.py
+++ pyutils/__init__.py
@@ -4,3 +4,12 @@
 __test_version__ = "0.0.0a0"
 # Version of package
 __version__ = "0.1.0a0"
+
+CONFIGS_DIRNAME = 'configs'
+MODEL_CONFIGS_DIRNAME = 'model_configs'
+MODEL_FNAME_ENDING = '_config.py'
+SKLEARN_MODULES = ['calibration', 'dummy', 'ensemble', 'gaussian_process',
+                   'linear_model', 'multiclass', 'multioutput', 'naive_bayes',
+                   'neighbors', 'neural_network', 'semi_supervised', 'svm',
+                   'tree']
+SKLEARN_MODULES.sort()
