e09b452dcfdf9bd6c455fde43d490606e9e25581
Raul <rchfe23@gmail.com>
2020-12-03 19:06:29 -0500

    train_model.py: add support for training multiple models sequentially

--- pyutils/default_scripts/train_model.py
+++ pyutils/default_scripts/train_model.py
@@ -18,36 +18,39 @@ def main():
     # ---------------------------------
     # Setup logging and get config dict
     # ---------------------------------
-    bp = ge.ConfigBoilerplate(__file__)
-    cfg_dict = bp.get_cfg_dict()
+    cfg_dicts = ge.ConfigBoilerplate(__file__).get_cfg_dict()
 
     # ---------
     # Load data
     # ---------
-    data = ml.Dataset(**cfg_dict)
+    data = ml.Dataset(**cfg_dicts[0])
 
-    # ---------
-    # Get model
-    # ---------
-    clf = ml.get_model(**cfg_dict['model'])
+    # For each model, get its config dict
+    for i, cfg_dict in enumerate(cfg_dicts, start=1):
+        # ---------
+        # Get model
+        # ---------
+        logger_data.info("")
+        logger.info(f"Model #{i}: {cfg_dict['model']['model_type']}")
+        clf = ml.get_model(**cfg_dict['model'])
 
-    # -----------
-    # Train model
-    # -----------
-    logger.info("Training model")
-    logger_data.debug(f"{clf}")
-    clf.fit(data.X_train, data.y_train)
-    score = clf.score(data.X_train, data.y_train)
-    logger.info(f"Score on train: {score}")
+        # -----------
+        # Train model
+        # -----------
+        logger.info("Training model")
+        logger_data.debug(f"{clf}")
+        clf.fit(data.X_train, data.y_train)
+        score = clf.score(data.X_train, data.y_train)
+        logger.info(f"Score on train: {score}")
 
-    # -----------------------
-    # Get predictions on test
-    # -----------------------
-    logger.info("Getting predictions from test data")
-    predictions = clf.predict(data.X_test)
-    if data.y_test is not None:
-        test_score = accuracy_score(data.y_test, predictions)
-        logger.info(f"Score on test: {test_score}")
+        # -----------------------
+        # Get predictions on test
+        # -----------------------
+        logger.info("Getting predictions from test data")
+        predictions = clf.predict(data.X_test)
+        if data.y_test is not None:
+            test_score = accuracy_score(data.y_test, predictions)
+            logger.info(f"Score on test: {test_score}")
 
 
 if __name__ == '__main__':
