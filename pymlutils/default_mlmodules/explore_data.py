476dbcfee6f1d340e21e980c7e56943f69e00178
Raul <rchfe23@gmail.com>
2020-12-08 20:36:03 -0500

    data_explore: use Bunch config

--- pymlutils/default_mlmodules/explore_data.py
+++ pymlutils/default_mlmodules/explore_data.py
@@ -2,19 +2,18 @@
 
 This module is used for doing data exploration of a dataset such as computing
 stats (e.g. mean, quantiles) and generating charts (e.g. bar chart and
-distribution graphs).
+distribution graphs) in order to better understand the dataset.
 """
 from pymlutils import dautils as da, genutils as ge
 
 logger = ge.init_log(__name__)
-logger_data = ge.init_log('data')
 
 
-def explore(cfg_dict):
+def explore(config):
     # ------------------------------------
     # Data exploration: compute stats, ...
     # ------------------------------------
-    data = da.DataExplorer(**cfg_dict)
+    data = da.DataExplorer(config=config)
     # For each column in the data (from train, test), list number of missing values
     data.count_null()
     # Compute stats (describe()) on each data (train, test)
