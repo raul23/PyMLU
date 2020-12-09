b1e1651dc8b9d2fd9b17986f1bbbc0248882f462
Raul <rchfe23@gmail.com>
2020-12-08 22:00:59 -0500

    Update __init__.py

--- pymlutils/__init__.py
+++ pymlutils/__init__.py
@@ -6,10 +6,12 @@ __test_version__ = "0.0.0a0"
 __version__ = "0.1.0a0"
 
 
+# TODO (IMPORTANT): should be in genutils
 CONFIGS_DIRNAME = 'mlconfigs'
 MODEL_CONFIGS_DIRNAME = 'model_configs'
 MODEL_FNAME_SUFFIX = '_config.py'
 MODULES_DIRNAME = 'mlmodules'
+# TODO (IMPORTANT): should be in mlutils
 # TODO: get categories from directory
 SKLEARN_MODULES = ['calibration', 'dummy', 'ensemble', 'gaussian_process',
                    'linear_model', 'multiclass', 'multioutput', 'naive_bayes',
