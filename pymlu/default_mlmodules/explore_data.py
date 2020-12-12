3d8eaff44af27759140c945e21befb29f94d3cb9
Raul <rchfe23@gmail.com>
2020-12-11 23:19:01 -0500

    explore_data: remove logger

--- pymlu/default_mlmodules/explore_data.py
+++ pymlu/default_mlmodules/explore_data.py
@@ -4,9 +4,7 @@ This module is used for doing data exploration of a dataset such as computing
 stats (e.g. mean, quantiles) and generating charts (e.g. bar chart and
 distribution graphs) in order to better understand the dataset.
 """
-from pymlu import dautils as da, genutils as ge
-
-logger = ge.init_log(__name__)
+from pymlu import dautils as da
 
 
 def explore(config):
